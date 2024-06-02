f1e314a6fd791293508c138ea49b5fdf4480c4c4
billingtonbill12 <166965277+billingtonbill12@users.noreply.github.com>
2024-06-02 10:12:26 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,79 @@
+#btree with object key and object val, maybe change to more specific stuff later
+#from https://btrees.readthedocs.io/en/latest/index.html
+import BTrees.OOBTree
+
+"""
+A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
+"""
+class MyBTree(BTrees.OOBTree.BTree):
+
+    max_leaf_size = 50000
+    max_internal_size = 10000
+
+class Index:
+    def __init__(self, table):
+        # One index for each column for fast search. All our empty initially.
+        self.table = table
+        self.indices = [None] *  table.num_columns
+        self.updated = [0] * table.num_columns
+        #creates index for key columnu
+        #create index for all
+        for i in range(table.num_columns):
+            self.create_index(i)
+        #if value got updated need to reindex
+        self.updated = [0] * table.num_columns
+        pass
+
+    """
+    # returns the location of all records with the given value on column "column"
+    """
+
+    def locate(self, column, value):
+        self.update_index(column)
+        return self.indices[column].get(value)
+
+    """
+    # Returns the RIDs of all records with values in column "column" between "begin" and "end"
+    """
+
+    def locate_range(self, begin, end, column):
+        self.update_index(column)
+        ridlists = self.indices[column].values(min = begin, max = end)
+        rv = []
+        for ridlist in ridlists:
+            rv.extend(ridlist)
+        return rv
+    """
+    # optional: Create index on specific column
+    """
+
+    def create_index(self, column_number):
+        self.drop_index(column_number)
+        self.indices[column_number] = MyBTree()
+        #figure out after we get organization done
+        for rid in self.table.baserids:
+            val = self.table.read_record(rid, 0)[column_number+7]
+            self.addToIndex(column_number, val, rid)
+
+    def addToIndex(self, column_number, val, rid):
+        #don't do anything if index doesn't exist
+        if(self.indices[column_number] == None):
+            return False
+        if self.indices[column_number].get(val) == None:
+            self.indices[column_number][val] = {rid}
+        else:
+            self.indices[column_number][val].add(rid)
+        return True
+    """
+    # optional: Drop index of specific column
+    """
+
+    def drop_index(self, column_number):
+        self.indices[column_number] = None
+        self.updated[column_number] = 0
+    
+    def update_index(self, column_number):
+        if self.indices[column_number] is None or self.updated[column_number] == 1:
+            self.drop_index(column_number)
+            self.create_index(column_number)
+            self.updated[column_number] = 0
---
+++ range.py
@@ -0,0 +1,165 @@
+from lstore.bufferpool import Bufferpool
+from lstore.page import Page
+from lstore.config import *
+
+class Range:
+
+    def __init__(self, num_columns, primary_key, table_name, range_number):
+
+        # set the number of columns
+        self.num_columns = num_columns
+
+        # set the column number of the primary key
+        self.primary_key = primary_key
+
+        # record the name of the table to which this range belongs
+        self.table_name = table_name
+
+        # record the number in the list of ranges that corresponds to this range
+        self.range_number = range_number
+
+        # set the number of records in this range
+        self.num_records = 0
+
+        # note the currently filling base page
+        self.current_base_page = -1
+
+        # currently filling tail page
+        self.current_tail_page = -1
+
+        # generate an initial empty base page
+        self.add_base_page()
+        self.add_tail_page()
+
+    def add_base_record(self, columns):
+        # update each of the columns
+        for column_num in range(self.num_columns):
+
+            # get the most recent page
+            page = Bufferpool().hold_base_page(self.table_name, self.range_number, column_num, self.current_base_page)
+
+            # if the page has no space to be written to
+            if page.has_capacity() != True:
+
+                # release the currently held page since it is not the one that we want
+                Bufferpool().release_base_page(self.table_name, self.range_number, column_num, self.current_base_page)
+
+                # then add a base page
+                self.add_base_page()
+
+                # load the new empty base page
+                page = Bufferpool().hold_base_page(self.table_name, self.range_number, column_num, self.current_base_page)
+
+            # write to it
+            offset = page.write(columns[column_num])
+
+            # release the page after it is no longer needed
+            Bufferpool().release_base_page(self.table_name, self.range_number, column_num, self.current_base_page)
+        
+        # increase the number of records
+        self.num_records += 1
+
+        # return the offset and page number within the page range
+        return self.current_base_page, offset
+    
+    def add_base_page(self):
+
+        # update the current base page value
+        self.current_base_page += 1
+
+        # add a new base page which means one more page in each of the columns
+        for column_num in range(self.num_columns):
+
+            # add a new base page
+            Bufferpool().add_base_page(self.table_name, self.range_number, column_num, self.current_base_page)
+    
+    def read_base_record(self, base_page_number, offset):
+
+        # declare a variable to hold the record contents
+        columns = []
+
+        # collect the value for each of the columns
+        for column_num in range(self.num_columns):
+
+            # read the value from the correct page
+            columns.append(Bufferpool().hold_base_page(self.table_name, self.range_number, column_num, base_page_number).read(offset))
+            Bufferpool().release_base_page(self.table_name, self.range_number, column_num, base_page_number)
+
+        # return the full record
+        return columns
+
+    def add_tail_record(self, columns):
+        # update each of the columns
+        for column_num in range(self.num_columns):
+
+            # get the most recent page
+            page = Bufferpool().hold_tail_page(self.table_name, self.range_number, column_num, self.current_tail_page)
+
+            # if the page has no space to be written to
+            if page.has_capacity() != True:
+
+                # release the currently held page since it is not the one that we want
+                Bufferpool().release_tail_page(self.table_name, self.range_number, column_num, self.current_tail_page)
+
+                # then add a tail page
+                self.add_tail_page()
+
+                # load the new empty tail page
+                page = Bufferpool().hold_tail_page(self.table_name, self.range_number, column_num, self.current_tail_page)
+
+            # write to it
+            offset = page.write(columns[column_num])
+
+            # release the page after it is no longer needed
+            Bufferpool().release_tail_page(self.table_name, self.range_number, column_num, self.current_tail_page)
+
+        # return the offset and page number within the page range
+        return self.current_tail_page, offset
+
+    def add_tail_page(self):
+
+        # update the current base page value
+        self.current_tail_page += 1
+
+        # add a new base page which means one more page in each of the columns
+        for column_num in range(self.num_columns):
+
+            # add a new tail page
+            Bufferpool().add_tail_page(self.table_name, self.range_number, column_num, self.current_tail_page)
+
+
+    def read_tail_record(self, tail_page_number, offset):
+        # declare a variable to hold the record contents
+        columns = []
+
+        # collect the value for each of the columns
+        for column_num in range(self.num_columns):
+
+            # read the value from the correct page
+            columns.append(Bufferpool().hold_tail_page(self.table_name, self.range_number, column_num, tail_page_number).read(offset))
+            Bufferpool().release_tail_page(self.table_name, self.range_number, column_num, tail_page_number)
+
+        # return the full record
+        return columns
+
+    def change_indirection(self, base_page_number, offset, value):
+        page = Bufferpool().hold_base_page(self.table_name, self.range_number, INDIRECTION_COLUMN, base_page_number)
+        page.write_at_offset(value, offset)
+        Bufferpool().release_base_page(self.table_name, self.range_number, INDIRECTION_COLUMN, base_page_number)
+        return True
+    
+    def change_schema_encoding(self, base_page_number, offset, value):
+        page = Bufferpool().hold_base_page(self.table_name, self.range_number, SCHEMA_ENCODING_COLUMN, base_page_number)
+        page.write_at_offset(value, offset)
+        Bufferpool().release_base_page(self.table_name, self.range_number, SCHEMA_ENCODING_COLUMN, base_page_number)
+        return True
+
+
+    def has_capacity(self):
+
+        # returns true if there is capacity, false otherwise
+        if self.num_records < 65536:
+            return True
+        
+        else:
+            return False
